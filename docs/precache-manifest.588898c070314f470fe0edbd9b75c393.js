ec1c3105631b639e6e65fb5dcbcb2d1ddd8ea089
Jon Harper <jon.harper87@gmail.com>
2019-03-13 16:13:28 +0100

    try to use ghpages

---
+++ docs/precache-manifest.588898c070314f470fe0edbd9b75c393.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecoaccu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "de1cd723f84ae067ab3c",
+    "url": "/ecoaccu/static/js/runtime~main.cad15769.js"
+  },
+  {
+    "revision": "45102ea029b5f6971ee6",
+    "url": "/ecoaccu/static/js/main.a9066056.chunk.js"
+  },
+  {
+    "revision": "3a06d567725789d19200",
+    "url": "/ecoaccu/static/js/2.81e718cf.chunk.js"
+  },
+  {
+    "revision": "45102ea029b5f6971ee6",
+    "url": "/ecoaccu/static/css/main.b737024f.chunk.css"
+  },
+  {
+    "revision": "3a06d567725789d19200",
+    "url": "/ecoaccu/static/css/2.e6f0f542.chunk.css"
+  },
+  {
+    "revision": "579214e96877969beffa2fb600a71f85",
+    "url": "/ecoaccu/index.html"
+  }
+];
